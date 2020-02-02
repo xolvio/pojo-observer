99bebc25478b7aef56df9dde5f211fc13bf0bbb8
Sam Hatoum <sam@xolv.io>
2020-02-02 12:31:14 -0500

    Fixes npm publish

--- index.js
+++
@@ -1,6 +0,0 @@
-import useDomain from './src/useDomain'
-import toCQRSWithHash from './src/toCQRSWithHash'
-import {command, query, hashable} from './src/decorators'
-
-export default useDomain
-export {toCQRSWithHash, command, query, hashable}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import useDomain from './useDomain'
+import toCQRSWithHash from './toCQRSWithHash'
+import {command, query, hashable} from './decorators'
+
+export default useDomain
+export {toCQRSWithHash, command, query, hashable}
