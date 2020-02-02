97bc0941a0563725914d2a9e0162d96fdc5fdb2f
Sam Hatoum <sam@xolv.io>
2020-02-02 12:43:22 -0500

    Fixes 'bound' naming issue

--- src/useDomain.js
+++ src/useDomain.js
@@ -12,28 +12,28 @@ export default function useDomain(model) {
 
   const domainModel = {queries: {}, commands: {}}
 
-  function attachQuery(query) {
-    domainModel.queries[query.name] = query
+  function attachQuery(query, name) {
+    domainModel.queries[name] = query
   }
 
-  function attachCommand(command) {
-    domainModel.commands[command.name] = (...args) => {
+  function attachCommand(command, name) {
+    domainModel.commands[name] = (...args) => {
       const returnValue = command.apply(model, args)
-      commandsHistory.push({command: command.name, args, id: commandsHistory.length})
+      commandsHistory.push({command: name, args, id: commandsHistory.length})
       stateChange(model.hash())
       return returnValue
     }
   }
 
   function useExplicitStrategy() {
-    Object.keys(model.queries).forEach((query) => attachQuery(model.queries[query]))
-    Object.keys(model.commands).forEach((command) => attachCommand(model.commands[command]))
+    Object.keys(model.queries).forEach((query) => attachQuery(model.queries[query], query))
+    Object.keys(model.commands).forEach((command) => attachCommand(model.commands[command], command))
   }
 
   function useDecoratorStrategy() {
     Object.keys(model).forEach((method) => {
-        if (model[method].query) attachQuery(model[method])
-        if (model[method].command) attachCommand(model[method])
+        if (model[method].query) attachQuery(model[method], method)
+        if (model[method].command) attachCommand(model[method], method)
       }
     )
   }
