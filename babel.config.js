e6b18a3197674469688cbab2063a82534b1a4640
lgandecki <lgandecki@thebrain.pro>
2020-02-03 17:57:15 +0400

    add typescript

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}], "@babel/preset-react"],
+  presets: [['@babel/preset-env', {targets: {node: 'current'}}], "@babel/preset-react", "@babel/typescript"],
   plugins: [
     ["@babel/plugin-proposal-decorators", { "legacy": true }],
     ["@babel/plugin-proposal-class-properties", { "legacy": true }],
