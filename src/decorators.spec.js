9b8c95529cf32c7423c0c51d393b5e5057ce58fb
Sam Hatoum <sam@xolv.io>
2020-02-02 21:41:03 -0500

    Fixes the suites

--- src/decorators.spec.js
+++ src/decorators.spec.js
@@ -24,22 +24,28 @@ class TestClass {
 const testClassInstance = new TestClass()
 
 describe('decorators', () => {
-  describe('commands', () => {
-    it('should provide a hash method on the class instance', function () {
+  describe('@hashable', () => {
+    it('should provide a hash method on instances of classes that have the @hashable decorator', function () {
       expect(typeof testClassInstance.hash()).toEqual('string')
     })
+  })
+  describe('@command', () => {
     it('should add a command attribute to methods that have been annotated with the @command decorator', () => {
       expect(testClassInstance.doSomething.command).toEqual(true)
       expect(testClassInstance.doSomethingElse.command).toEqual(true)
     })
+    it('should not add any command attributes to methods that have not had the @command decorator', function () {
+      expect(testClassInstance._aPrivateMethod.command).toEqual(undefined)
+      expect(testClassInstance.aPublicMethod.command).toEqual(undefined)
+    })
+  })
+  describe('@query', () => {
     it('should add a query attribute to methods that have been annotated with the @query decorator', () => {
       expect(testClassInstance.showSomething.query).toEqual(true)
       expect(testClassInstance.showSomethingElse.query).toEqual(true)
     })
-    it('should not add any command or query attributes to methods that have not had the @command or @query decorators', function () {
-      expect(testClassInstance._aPrivateMethod.command).toEqual(undefined)
+    it('should not add any query attributes to methods that have not had the @query decorator', function () {
       expect(testClassInstance._aPrivateMethod.query).toEqual(undefined)
-      expect(testClassInstance.aPublicMethod.command).toEqual(undefined)
       expect(testClassInstance.aPublicMethod.query).toEqual(undefined)
     })
   })
