2748fc04adf6fbf0de3e180ef49b8fa8b741802a
Sam Hatoum <sam@xolv.io>
2020-02-05 13:27:47 -0500

    Formats the file

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}], "@babel/preset-react", "@babel/typescript"],
+  presets: [
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    '@babel/preset-react',
+    '@babel/typescript'
+  ],
   plugins: [
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+    ['@babel/plugin-proposal-class-properties', {loose: true}]
   ]
-};
+}
