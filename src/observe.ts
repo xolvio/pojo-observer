1c289982889e201603df3ef0f4deb66907fe51b3
Sam Hatoum <sam@xolv.io>
2020-02-14 10:00:11 -0500

    Fix linting errors + one dirty commit addition to add new edge cases

--- src/observe.ts
+++ src/observe.ts
@@ -1,6 +1,25 @@
 import {useCallback, useEffect, useState} from 'react'
 import hash from './hash'
 
+class EventEmitter {
+  callbacks = {}
+
+  on(eventId, cb): void {
+    this.callbacks[eventId] = this.callbacks[eventId] || []
+    this.callbacks[eventId].push(cb)
+  }
+
+  remove(eventId): void {
+    delete this.callbacks[eventId]
+  }
+
+  emit(eventId): void {
+    this.callbacks[eventId] && this.callbacks[eventId].forEach(cb => cb())
+  }
+}
+
+const eventEmitter = new EventEmitter()
+
 type Model = {
   constructor
   __observableId?: string
@@ -8,27 +27,72 @@ type Model = {
   hash?: () => string
 }
 
-const id = () =>
+const id = (): string =>
   'xxxxxxxxxxxxxxxx'.replace(/[x]/g, () =>
-    ((Math.floor(new Date().getTime() / 16) + Math.random() * 16) % 16 | 0 & 0x3 | 0x8).toString(16))
+    (
+      (Math.floor(new Date().getTime() / 16) + Math.random() * 16) % 16 |
+      (0 & 0x3) |
+      0x8
+    ).toString(16)
+  )
 
-
-function getFields(toCheck) {
+function getFieldNames(toCheck): string[] {
   let props = []
   let obj = toCheck
 
   do {
     props = props.concat(Object.getOwnPropertyNames(obj))
   } while ((obj = Object.getPrototypeOf(obj)))
-  return props.sort().filter((e, i, arr) => (e != arr[i + 1] && typeof toCheck[e] !== 'function'))
+  return props
+    .sort()
+    .filter((e, i, arr) => e != arr[i + 1] && typeof toCheck[e] !== 'function')
 }
 
-function attachProxyToField(object, fieldName, originalField, id) {
+function isWritableField(object, fieldName): boolean {
+  const fieldDescriptor = Object.getOwnPropertyDescriptor(object, fieldName)
+  return fieldDescriptor && fieldDescriptor.writable
+}
+
+function isWriteableObjectField(object, fieldName): boolean {
+  return (
+    isWritableField(object, fieldName) &&
+    typeof object[fieldName] === 'object' &&
+    object[fieldName] !== null
+  )
+}
+
+function isWriteablePrimitiveField(object, fieldName): boolean {
+  return (
+    isWritableField(object, fieldName) &&
+    (typeof object[fieldName] !== 'object' || object[fieldName] === null)
+  )
+}
+
+function isWriteableArray(object, fieldName): boolean {
+  return isWritableField(object, fieldName) && Array.isArray(object[fieldName])
+}
+
+function attachProxyToProperties<T extends Model>(model: T, id?): void {
+  if (!model.__proxyAttached) {
+    model.__proxyAttached = true
+    getFieldNames(model).forEach(field => {
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      recursivelyAttachProxy(
+        model[field],
+        field,
+        model,
+        id ? id : model.__observableId
+      )
+    })
+  }
+}
+
+function attachProxyToField(object, fieldName, originalField, id): void {
   Object.defineProperty(object, fieldName, {
     configurable: true,
     enumerable: true,
     get: () => originalField,
-    set: (value) => {
+    set: value => {
       if (typeof value === 'object') {
         attachProxyToProperties(value, id)
       }
@@ -38,68 +102,53 @@ function attachProxyToField(object, fieldName, originalField, id) {
   })
 }
 
-function attachProxyToArray(object, fieldName, id) {
+function attachProxyToArray(object, fieldName, id): void {
   object[fieldName] = new Proxy(object[fieldName], {
-    get: function (target, property) {
-      return target[property];
+    get: function(target, property): object {
+      return target[property]
     },
-    set: function (target, property, value) {
-
+    set: function(target, property, value): boolean {
       if (property !== '__proto__' && property !== 'length') {
-        target[property] = value;
+        target[property] = value
         eventEmitter.emit(id)
       }
-      return true;
+      return true
     }
   })
 }
 
-function isWritableField(object, fieldName) {
-  const fieldDescriptor = Object.getOwnPropertyDescriptor(object, fieldName)
-  return fieldDescriptor && fieldDescriptor.writable
-}
-
-function isWriteableObjectField(object, fieldName) {
-  return isWritableField(object, fieldName) && typeof object[fieldName] === 'object' && object[fieldName] !== null
-}
-
-function isWriteablePrimitiveField(object, fieldName) {
-  return isWritableField(object, fieldName) && (typeof object[fieldName] !== 'object' || object[fieldName] === null)
-}
-
-function isWriteableArray(object, fieldName) {
-  return isWritableField(object, fieldName) && Array.isArray(object[fieldName])
-}
-
-function recursivelyAttachProxy(originalField, fieldName, object, id) {
-  if (isWriteablePrimitiveField(object, fieldName)) return attachProxyToField(object, fieldName, originalField, id)
-  if (isWriteableArray(object, fieldName)) return attachProxyToArray(object, fieldName, id)
+function recursivelyAttachProxy(originalField, fieldName, object, id): void {
+  if (isWriteablePrimitiveField(object, fieldName))
+    return attachProxyToField(object, fieldName, originalField, id)
+  if (isWriteableArray(object, fieldName))
+    return attachProxyToArray(object, fieldName, id)
   if (isWriteableObjectField(object, fieldName)) {
     attachProxyToField(object, fieldName, originalField, id)
-    getFields(object[fieldName]).forEach((nestedFieldName) =>
-      recursivelyAttachProxy(object[fieldName][nestedFieldName], nestedFieldName, object[fieldName], id))
+    getFieldNames(object[fieldName]).forEach(nestedFieldName =>
+      recursivelyAttachProxy(
+        object[fieldName][nestedFieldName],
+        nestedFieldName,
+        object[fieldName],
+        id
+      )
+    )
     return
   }
 }
 
-function attachProxyToProperties<T extends Model>(model: T, id?) {
-  if (!model.__proxyAttached) {
-    model.__proxyAttached = true
-    getFields(model).forEach(field => {
-      recursivelyAttachProxy(model[field], field, model, id ? id : model.__observableId)
+function addId<T extends Model>(model: T): void {
+  if (!model.__observableId)
+    Object.defineProperty(model, '__observableId', {
+      value: id(),
+      writable: false
     })
-  }
 }
 
-function addId<T extends Model>(model: T) {
-  if (!model.__observableId) Object.defineProperty(model, '__observableId', {value: id(), writable: false})
+function addHash<T extends Model>(model: T): void {
+  if (!model.hash) model.hash = (): string => hash(model)
 }
 
-function addHash<T extends Model>(model: T) {
-  if (!model.hash) model.hash = () => hash(model)
-}
-
-function reactify<T extends Model>(model: T) {
+function reactify<T extends Model>(model: T): void {
   const [, stateChange] = useState(model.hash())
 
   const stateChangeCallback = useCallback(() => {
@@ -108,32 +157,17 @@ function reactify<T extends Model>(model: T) {
 
   useEffect(() => {
     eventEmitter.on(model.__observableId, stateChangeCallback)
-    return () => eventEmitter.remove(model.__observableId)
+    return (): void => eventEmitter.remove(model.__observableId)
   }, [model.__observableId])
 }
 
-class EventEmitter {
-  callbacks = {}
-
-  on(eventId, cb) {
-    this.callbacks[eventId] = this.callbacks[eventId] || []
-    this.callbacks[eventId].push(cb)
-  }
-
-  remove(eventId) {
-    delete this.callbacks[eventId]
-  }
-
-  emit(eventId) {
-    this.callbacks[eventId] && this.callbacks[eventId].forEach(cb => cb())
-  }
-}
-
-const eventEmitter = new EventEmitter()
-
-function observe<T extends Model>(model: T): T {
+function decorate<T extends Model>(model: T): void {
   addHash(model)
   addId(model)
+}
+
+function observe<T extends Model>(model: T): T {
+  decorate(model)
   attachProxyToProperties(model)
   reactify(model)
   return model
