f9a85e7918bd4884dc529db0e290ec180bcd451b
lgandecki <lgandecki@thebrain.pro>
2020-02-15 13:05:17 +0400

    pure observer implemented for better code seperation/testing

---
+++ src/pureObserver.spec.ts
@@ -0,0 +1,48 @@
+import * as td from 'testdouble'
+import {pureObserver} from './useObserver'
+
+test('it reacts to a change', () => {
+  const object = {test: 'abc'}
+  const obj1Callback = td.func() as () => void
+  pureObserver(object, obj1Callback)
+
+  object.test = 'def'
+  td.verify(obj1Callback())
+  object.test = 'there'
+  td.verify(obj1Callback(), {times: 2})
+})
+
+test('that it can work with multiple objects, no react', () => {
+  const obj1 = {
+    foo: 'here',
+    mutateMe: () => (obj1.foo = 'there')
+  }
+  const obj2 = {
+    bar: 'pete',
+    mutateMe: () => (obj2.bar = 'paul')
+  }
+  const obj1Callback = td.func() as () => void
+  pureObserver(obj1, obj1Callback)
+  const obj2Callback = td.func() as () => void
+  pureObserver(obj2, obj2Callback)
+
+  obj1.mutateMe()
+  td.verify(obj1Callback())
+  td.verify(obj2Callback(), {times: 0})
+  obj2.mutateMe()
+  td.verify(obj2Callback())
+})
+
+test('it should callback for changes in objects added to arrays', () => {
+  const object = {arr: []}
+  const obj1Callback = td.func()
+  pureObserver(object, obj1Callback)
+
+  object.arr[0] = {
+    hello: 'world'
+  }
+  td.verify(obj1Callback())
+
+  object.arr[0].hello = 'there'
+  td.verify(obj1Callback(), {times: 2})
+})
