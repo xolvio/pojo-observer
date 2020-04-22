e0d0dfbe4b4358901a0d3f725ccf05f88baa4b15
Łukasz Gandecki <4002543+lgandecki@users.noreply.github.com>
2020-04-22 13:17:03 +0200

    lint passing

--- example/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const App = () => (<div>Hello world </div>)
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
---
+++ example/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <div>Hello world </div>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
